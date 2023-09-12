18a7d25c1e48613cbec3acb160c04b3de30cadfc
MireiaCastillejo <mireia.castillejo@students.salle.url.edu>
2023-09-12 17:01:51 +0200

    final

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -18,7 +18,6 @@ function FeaturesZigzag() {
               <div className="timeline-content right max-w-xl md:max-w-none md:w-full mx-auto" data-aos="fade-right">
                 <h3 className="h3 mb-3">Engineer</h3>
                 <h3 className="h6 mb-6">NTT DATA Europe & Latam</h3>
-                <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <ul className="text-lg text-gray-400 -mb-2">
                   <li className="flex items-center mb-2">
                     <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
@@ -51,7 +50,6 @@ function FeaturesZigzag() {
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <h3 className="h3 mb-3">Junior Engineer</h3>
                   <h3 className="h6 mb-6">NTT DATA Europe & Latam</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
@@ -84,7 +82,6 @@ function FeaturesZigzag() {
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <h3 className="h3 mb-3">Solution Assistant</h3>
                   <h3 className="h6 mb-6">Everis</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
